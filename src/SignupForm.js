064616b9e7f6f1b564ecc9da85e5a8c1c07fde5c
unknown <arshaskrd97@gmail.com>
2021-05-25 17:58:10 +0530

    Contact Form

---
+++ src/SignupForm.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react';
+
+import Validation from './Validation';
+
+const SignupForm = ({ submitForm }) => {
+  const [values, setValues] = useState({
+    fullname: '',
+    email: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState({});
+  const [dataIsCorrect, setDataIsCorrect] = useState(false);
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setErrors(Validation(values));
+    setDataIsCorrect(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && dataIsCorrect) {
+      submitForm(true);
+    }
+  }, [errors]);
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <h2 className="title">Contact Form</h2>
+        </div>
+        <form className="form-wrapper">
+          <div className="name">
+            <label className="label">Full Name</label>
+            <input
+              className="input"
+              type="text"
+              name="fullname"
+              value={values.fullname}
+              onChange={handleChange}
+            />
+            {errors.fullname && <p className="error">{errors.fullname}</p>}
+          </div>
+          <div className="email">
+            <label className="label">Email</label>
+            <input
+              className="input"
+              type="email"
+              name="email"
+              values={values.email}
+              onChange={handleChange}
+            />
+            {errors.email && <p className="error">{errors.email}</p>}
+          </div>
+          <div className="password">
+            <label className="label">Password</label>
+            <input
+              className="input"
+              type="password"
+              name="password"
+              values={values.password}
+              onChange={handleChange}
+            />
+            {errors.password && <p className="error">{errors.password}</p>}
+          </div>
+          <div>
+            <button className="submit" onClick={handleFormSubmit}>
+              SignUp
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default SignupForm;
---
+++ src/SignupFormSuccess.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React from 'react';
+
+const SignupFormSuccess = () => {
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <h1 className="form-success">Submit Successfully</h1>
+      </div>
+    </div>
+  );
+};
+export default SignupFormSuccess;
